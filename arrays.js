7b590e70d9a71393a6b866d0b0ffbc5fa8cc2213
Luis Ascencio <luismiguelascencio@gmail.com>
2019-02-18 16:27:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,4 +6,4 @@ var ingredient5 = "tomato"
 var ingredient6 = "garlic"
 
 var chocolateBars= ["snicker", "hundred grand", "kitkat", "skittles"]
-Function addElementToBeginningOfArray 
+Function addElementToBeginningOfArray (Array,eleme)
